ed1ffd6262abac2e158bf735ed804255dd59dde2
Bárbara Ayres <ayresbarbara2@gmail.com>
2022-12-21 17:37:04 -0300

    Atividades

--- src/aula3/Calculadora.java
+++ src/aula3/Calculadora.java
@@ -8,7 +8,7 @@ public class Calculadora {
 			for (int j = 1; j<=10; j++) {
 				System.out.println(j + " x " + i + " = " + j * i);
 			}
-			System.out.println("\n");
+			System.out.println();
 
 	}
 }
